77c0c3b7759476a2352094319ac9d87e1e7cf503
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-04 23:50:54 +0530

    Added: welcome message

---
+++ src/com/blabz/SnakeLadder/SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.blabz.SnakeLadder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		System.out.println("welcome to snake and ladder programming");
+		
+	}
+
+}
